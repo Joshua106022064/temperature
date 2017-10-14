f53bde41c116baace2a737f726ec6411112ac60b
unknown <joshua310041@gmail.com>
2017-10-14 23:53:02 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        float x;
+        x=in.nextFloat();
+        System.out.printf("%f",x*9/5+32);
+        System.out.println();
+
+    }
+}
